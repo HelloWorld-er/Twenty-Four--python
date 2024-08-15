ced1193b927bed9464213c93e41df1f4c9b1772f
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2024-08-15 20:22:57 +0800

    update fonts style sheet

--- fonts.py
+++ fonts.py
@@ -5,5 +5,13 @@ def get_font(family="Arial", size=14, weight="normal", slant="roman"):
 	return customtkinter.CTkFont(family=family, size=size, weight=weight, slant=slant)
 
 
-def get_heading(size=18):
+def get_heading(size=26):
 	return get_font(size=size, weight="bold")
+
+
+def get_aside_bar_default(size=20):
+	return get_font(size=size)
+
+
+def get_game_mode_button(size=18):
+	return get_font(size=size)
