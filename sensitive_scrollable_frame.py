041d945b131ee3fbec2bcb56c90898d11725ab70
LYMwyh <122608577+LYMwyh@users.noreply.github.com>
2024-08-18 23:07:19 +0800

    update sensitive_scrollable_frame.py

--- sensitive_scrollable_frame.py
+++ sensitive_scrollable_frame.py
@@ -2,63 +2,85 @@ import customtkinter
 
 
 class SensitiveScrollableFrame(customtkinter.CTkScrollableFrame):
+	_scrollbar_update_time = 200
+	
 	def __init__(self, parent, *args, **kwargs):
 		super().__init__(parent, *args, **kwargs)
+		self._hide_x_scrollbar = True
+		self._hide_y_scrollbar = True
+		if self._orientation == "horizontal":
+			self._create_grid(re_grid_frame=True, re_grid_x_scrollbar=True)
+		elif self._orientation == "vertical":
+			self._create_grid(re_grid_frame=True, re_grid_y_scrollbar=True)
+		self.after(50, self._check_if_scrollbars_needed, None, True)
 	
-	def _create_grid(self):
+	def _create_grid(self, re_grid_frame=False, re_grid_x_scrollbar=False, re_grid_y_scrollbar=False):
 		border_spacing = self._apply_widget_scaling(
 			self._parent_frame.cget("corner_radius") + self._parent_frame.cget("border_width"))
 		
-		if self._orientation == "horizontal":
+		if re_grid_frame:
 			self._parent_frame.grid_columnconfigure(0, weight=1)
 			self._parent_frame.grid_rowconfigure(1, weight=1)
-			self._parent_canvas.grid(row=1, column=0, sticky="nsew", padx=border_spacing, pady=(border_spacing, 0))
-			self._update_scrollbar(border_spacing)
+			self._parent_canvas.grid(row=1, column=0, sticky="nsew")
+		
+		if re_grid_x_scrollbar:
+			if self._hide_x_scrollbar:
+				self._parent_canvas.grid_configure(padx=border_spacing, pady=border_spacing)
+				self._scrollbar.grid_forget()
+			else:
+				self._parent_canvas.grid_configure(padx=border_spacing, pady=(border_spacing, 0))
+				self._scrollbar.grid(row=2, column=0, sticky="nsew", padx=border_spacing)
 			
 			if self._label_text is not None and self._label_text != "":
 				self._label.grid(row=0, column=0, sticky="ew", padx=border_spacing, pady=border_spacing)
 			else:
 				self._label.grid_forget()
 		
-		elif self._orientation == "vertical":
-			self._parent_frame.grid_columnconfigure(0, weight=1)
-			self._parent_frame.grid_rowconfigure(1, weight=1)
-			self._parent_canvas.grid(row=1, column=0, sticky="nsew", padx=(border_spacing, 0), pady=border_spacing)
-			self._update_scrollbar(border_spacing)
+		if re_grid_y_scrollbar:
+			if self._hide_y_scrollbar:
+				self._parent_canvas.grid_configure(padx=border_spacing, pady=border_spacing)
+				self._scrollbar.grid_forget()
+			else:
+				self._parent_canvas.grid_configure(padx=(border_spacing, 0), pady=border_spacing)
+				self._scrollbar.grid(row=1, column=1, sticky="nsew", pady=border_spacing)
 			
 			if self._label_text is not None and self._label_text != "":
 				self._label.grid(row=0, column=0, columnspan=2, sticky="ew", padx=border_spacing, pady=border_spacing)
 			else:
 				self._label.grid_forget()
 	
-	def _fit_frame_dimensions_to_canvas(self, event):
+	def _check_if_scrollbars_needed(self, event=None, continue_loop: bool = False):
 		if self._orientation == "horizontal":
-			self._parent_canvas.itemconfigure(self._create_window_id, height=self._parent_canvas.winfo_height())
-			if self._parent_canvas.xview() != (0.0, 1.0):
-				self._update_scrollbar()
+			if self._parent_canvas.xview() != (0.0, 1.0) and not self._scrollbar.winfo_ismapped():  # x scrollbar needed
+				self._hide_x_scrollbar = False
+				self._create_grid(re_grid_x_scrollbar=True)
+			elif self._parent_canvas.xview() == (0.0, 1.0) and self._scrollbar.winfo_ismapped():  # x scrollbar not needed
+				self._hide_x_scrollbar = True
+				self._create_grid(re_grid_x_scrollbar=True)
 		elif self._orientation == "vertical":
-			self._parent_canvas.itemconfigure(self._create_window_id, width=self._parent_canvas.winfo_width())
-			if self._parent_canvas.yview() != (0.0, 1.0):
-				self._update_scrollbar()
+			# print(self._hide_y_scrollbar, self._parent_canvas.yview())
+			if self._parent_canvas.yview() != (0.0, 1.0) and not self._scrollbar.winfo_ismapped():  # y scrollbar needed
+				self._hide_y_scrollbar = False
+				self._create_grid(re_grid_y_scrollbar=True)
+			elif self._parent_canvas.yview() == (0.0, 1.0) and self._scrollbar.winfo_ismapped():  # y scrollbar not needed
+				self._hide_y_scrollbar = True
+				self._create_grid(re_grid_y_scrollbar=True)
+		
+		if self._parent_canvas.winfo_exists() and continue_loop is True:
+			self.after(self._scrollbar_update_time, lambda: self._check_if_scrollbars_needed(continue_loop=True))
 	
-	def _update_scrollbar(self, border_spacing=None):
-		if border_spacing is None:
-			border_spacing = self._apply_widget_scaling(
-				self._parent_frame.cget("corner_radius") + self._parent_frame.cget("border_width"))
-		if self._orientation == "horizontal":
-			if self._parent_canvas.xview() != (0.0, 1.0):
-				self._parent_canvas.grid_configure(padx=border_spacing, pady=(border_spacing, 0))
-				self._scrollbar.grid(row=2, column=0, sticky="nsew", padx=border_spacing)
-			else:
-				self._parent_canvas.grid_configure(padx=border_spacing, pady=border_spacing)
-				self._scrollbar.grid_remove()
-		elif self._orientation == "vertical":
-			if self._parent_canvas.yview() != (0.0, 1.0):
-				self._parent_canvas.grid_configure(padx=(border_spacing, 0), pady=(border_spacing, 0))
-				self._scrollbar.grid(row=1, column=1, sticky="nsew", padx=border_spacing)
-			else:
-				self._parent_canvas.grid_configure(padx=border_spacing, pady=(border_spacing, 0))
-				self._scrollbar.grid_remove()
+	def _set_dimensions(self, width=None, height=None):
+		super()._set_dimensions(width, height)
+		self._create_grid(re_grid_frame=True, re_grid_x_scrollbar=True,
+		                                           re_grid_y_scrollbar=True)
+	
+	def configure(self, **kwargs):
+		super().configure(**kwargs)
+		self._create_grid(re_grid_frame=True, re_grid_x_scrollbar=True,
+		                                           re_grid_y_scrollbar=True)
+	
+	def destroy(self):
+		super().destroy()
 	
 	def get_real_time_canvas_width(self):
 		return self._parent_canvas.winfo_width()
@@ -74,10 +96,10 @@ if __name__ == "__main__":
 	
 	app.columnconfigure(0, weight=1)
 	
-	frame = SensitiveScrollableFrame(app)
+	frame = SensitiveScrollableFrame(app, width=100, height=100)
 	frame.grid(row=0, column=0, sticky="nsew")
 	
-	for i in range(30):
+	for i in range(4):
 		label = customtkinter.CTkLabel(frame, text=f"Label {i}")
 		label.grid(row=i, column=0, sticky="ew", padx=5, pady=5)
 	
